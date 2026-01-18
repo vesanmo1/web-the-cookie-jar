707bd2d018932d2cbba55d73b4d2ab43ff91a8c7
vesanmo1 <veronicasanchez.design@gmail.com>
2026-01-18 19:34:44 +0100

    Remove form reset after updating cookies in CookiesProvider

--- src/context/CookiesContext.jsx
+++ src/context/CookiesContext.jsx
@@ -290,9 +290,6 @@ export const CookiesProvider = ( props ) => {
         // Guardamos la lista actualizada si viene en answer.data
         if (answer?.data) setCookies(answer.data)
 
-        // Limpiamos el formulario
-        putForm.current.reset()
-
         return answer
     }
 
